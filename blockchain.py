a0af48b14e5b25cf3d1318d57182510131a8592a
azizyilmaz <azizxyilmaz@outlook.com>
2020-05-04 01:02:57 +0300

    I started the blockchain application and created blockchain, block, mining block, genesis block classes and the hashing algorithm.

--- blockchain.py
+++ blockchain.py
@@ -1,25 +1,25 @@
-class Block:
-    def __init__(self, data):
-        self.data = data
-
-    def __repr__(self):
-        return f'Block - data: {self.data}'
+from block import Block
 
 
 class Blockchain:
     def __init__(self):
-        self.chain = []
+        self.chain = [Block.genesis()]
 
     def add_block(self, data):
-        self.chain.append(Block(data))
+        self.chain.append(Block.mine_block(self.chain[-1], data))
 
     def __repr__(self):
         return f'Blockchain: {self.chain}'
 
 
-blockchain = Blockchain()
-blockchain.add_block('one')
-blockchain.add_block('two')
-blockchain.add_block('three')
+def main():
+    blockchain = Blockchain()
+    blockchain.add_block('one')
+    blockchain.add_block('two')
+    blockchain.add_block('three')
 
-print(blockchain)
+    print(blockchain)
+
+
+if __name__ == '__main__':
+    main()
