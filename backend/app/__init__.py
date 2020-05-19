253a5bbb3734db1a930a173ab44da6c5fd66f219
azizyilmaz <azizxyilmaz@outlook.com>
2020-05-20 02:13:47 +0300

    Mine Block Request

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
+from backend.blockchain.blockchain import Blockchain
 
 app = Flask(__name__)
+blockchain = Blockchain()
 
 
 @app.route('/')
-def default():
+def route_default():
     return '<h1>Welcome to the blockchain</h1>'
+
+
+@app.route('/blockchain')
+def route_blockchain():
+    return jsonify(blockchain.to_json())
+
+
+@app.route('/blockchain/mine')
+def route_blockchain_mine():
+    transaction_data = 'stubbed_transaction_data'
+
+    blockchain.add_block(transaction_data)
+
+    return jsonify(blockchain.chain[-1].to_json())
 
 
 app.run()
