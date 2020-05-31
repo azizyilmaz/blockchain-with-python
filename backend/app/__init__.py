f561c8137c660b5325db9e04f91740dc1c08a312
azizyilmaz <azizxyilmaz@outlook.com>
2020-05-31 23:24:23 +0300

    We have transactions within blocks

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -29,12 +29,10 @@ def route_blockchain():
 
 @app.route('/blockchain/mine')
 def route_blockchain_mine():
-    transaction_data = 'stubbed_transaction_data'
-
-    blockchain.add_block(transaction_data)
-
+    blockchain.add_block(transaction_pool.transaction_data())
     block = blockchain.chain[-1]
     pubsub.broadcast_block(block)
+    transaction_pool.clear_blockchain_transactions(blockchain)
 
     return jsonify(block.to_json())
 
@@ -42,11 +40,20 @@ def route_blockchain_mine():
 @app.route('/wallet/transact', methods=['POST'])
 def route_wallet_transact():
     transaction_data = request.get_json()
-    transaction = Transaction(
-        wallet,
-        transaction_data['recipient'],
-        transaction_data['amount']
-    )
+    transaction = transaction_pool.existing_transaction(wallet.address)
+
+    if transaction:
+        transaction.update(
+            wallet,
+            transaction_data['recipient'],
+            transaction_data['amount']
+        )
+    else:
+        transaction = Transaction(
+            wallet,
+            transaction_data['recipient'],
+            transaction_data['amount']
+        )
 
     pubsub.broadcast_transaction(transaction)
 
